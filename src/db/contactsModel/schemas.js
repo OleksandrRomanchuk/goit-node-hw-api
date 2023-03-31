956246d6e2c2e284ab6e93b915e06fad4b7db710
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-31 17:44:37 +0300

    change structure

---
+++ src/db/contactsModel/schemas.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const contacts = new Schema({
+  name: {
+    type: String,
+    required: [true, "Set name for contact"],
+    unique: true,
+  },
+  email: {
+    type: String,
+  },
+  phone: {
+    type: String,
+  },
+  favorite: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+const Contacts = mongoose.model("contacts", contacts);
+
+module.exports = Contacts;
