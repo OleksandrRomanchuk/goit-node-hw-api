c37513cdf2d779240e283ff5e5f40380ef659779
Oleksandr Romanchuk <novator0511@gmail.com>
2023-03-31 11:44:14 +0300

    Update contactsController.js

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,19 +1,19 @@
 const {
-  listContacts,
+  getAllContacts,
   getContactById,
   addContact,
   updateContact,
-  removeContact,
-} = require("../models/contacts.js");
+  deleteContact,
+} = require("../models/contacts");
 const { HTTPError } = require("../helpers");
 
 const getAll = async (_, res) => {
-  const contacts = await listContacts();
+  const contacts = await getAllContacts();
 
   res.status(200).json(contacts);
 };
 
-const getById = async ({ params }, res) => {
+const getById = async ({ params }, res, _) => {
   const id = params.contactId;
   const contact = await getContactById(id);
 
@@ -22,18 +22,15 @@ const getById = async ({ params }, res) => {
   res.status(200).json(contact);
 };
 
-const add = async (req, res) => {
+const add = async (req, res, _) => {
   const body = req.body;
 
   const contact = await addContact(body);
 
-  if (!contact)
-    throw HTTPError(400, "A contact with such name already exists.");
-
   res.status(201).json(contact);
 };
 
-const update = async (req, res) => {
+const update = async (req, res, _) => {
   const id = req.params.contactId;
   const body = req.body;
 
@@ -44,14 +41,25 @@ const update = async (req, res) => {
   res.status(200).json(updatedContact);
 };
 
-const remove = async (req, res) => {
+const remove = async (req, res, _) => {
   const id = req.params.contactId;
 
-  const result = await removeContact(id);
+  const result = await deleteContact(id);
 
   if (!result) throw HTTPError(404, "Not found");
 
-  res.status(200).json(result);
+  res.status(200).json({ message: "Contact deleted." });
+};
+
+const updateStatus = async (req, res) => {
+  const id = req.params.contactId;
+  const body = req.body;
+
+  const updatedContact = await updateContact(id, body);
+
+  if (!updatedContact) throw HTTPError(404, "Not found");
+
+  res.status(200).json(updatedContact);
 };
 
 module.exports = {
@@ -60,4 +68,5 @@ module.exports = {
   add,
   update,
   remove,
+  updateStatus,
 };
