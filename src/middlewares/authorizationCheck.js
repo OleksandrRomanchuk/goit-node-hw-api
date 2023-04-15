48191571f7b62bbcc57393f1862a527d77404c91
Oleksandr Romanchuk <novator0511@gmail.com>
2023-04-15 12:37:31 +0300

    Create authorizationCheck.js

---
+++ src/middlewares/authorizationCheck.js
@@ -0,0 +1,37 @@
+const jwt = require("jsonwebtoken");
+const User = require("../services/userServices/userSchemas");
+const { HTTPError } = require("../helpers");
+const { SECRET_KEY } = process.env;
+
+const authorizationCheck = async (req, _, next) => {
+  const { authorization = "" } = req.headers;
+
+  if (!authorization) throw HTTPError(401, "Not authorized");
+
+  const [bearer, token] = authorization.split(" ");
+
+  if (bearer !== "Bearer") throw HTTPError(401, "Not authorized");
+
+  try {
+    const { userId } = jwt.verify(token, SECRET_KEY);
+    console.log("userId: ", userId);
+
+    if (!userId) throw HTTPError(401, "Not authorized");
+
+    const user = await User.findById(
+      { _id: userId },
+      "email subscription token"
+    );
+
+    if (!user || !user.token || user.token !== token)
+      throw HTTPError(401, "Not authorized");
+
+    req.user = user;
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = authorizationCheck;
